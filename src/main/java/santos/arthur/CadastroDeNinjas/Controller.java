df2d0d4e6a4901d1e1bcbd88290f7255659691d8
Arthur Santos <arthurdemarchisantos@gmail.com>
2025-11-18 19:28:00 -0300

    Controller criado

---
+++ src/main/java/santos/arthur/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package santos.arthur.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Seja Bem vindo, esta é minha primeira mensagem";
+    }
+
+}
