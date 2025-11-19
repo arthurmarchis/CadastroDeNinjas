ae6816b21bfbe36b82ac31d0ab11ee56dbba374d
Arthur Santos <arthurdemarchisantos@gmail.com>
2025-11-19 16:38:32 -0300

    fix: Coloca a coluna email como UNIQUE

--- src/main/java/santos/arthur/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/santos/arthur/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -17,8 +17,12 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
 
     @ManyToOne
