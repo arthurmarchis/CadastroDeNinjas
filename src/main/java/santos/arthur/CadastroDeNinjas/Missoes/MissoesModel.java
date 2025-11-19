a8bf97d9505cef7da190393d80f6668f30b5acfc
Arthur Santos <arthurdemarchisantos@gmail.com>
2025-11-19 16:35:11 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/santos/arthur/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/santos/arthur/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,12 +1,18 @@
 package santos.arthur.CadastroDeNinjas.Missoes;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import santos.arthur.CadastroDeNinjas.Ninjas.NinjaModel;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -17,25 +23,5 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissoesModel(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 
 }
